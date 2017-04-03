5935de7f00b0b1e0131c11c86f86d0d057a461a7
Surabh Shah <surabh.shah@mobiliya.com>
2017-04-03 14:39:17 +0530

    Add file Writing

--- CICDFirstAttempt/CICDFirstAttempt/Program.cs
+++ CICDFirstAttempt/CICDFirstAttempt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,43 @@ namespace CICDFirstAttempt
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!");
+            CreateFile();
+        }
+
+        private static void CreateFile()
+        {
+            string fileName = @"C:\CICD\CICD.txt";
+
+            try
+            {
+                // Check if file already exists. If yes, delete it. 
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                // Create a new file 
+                using (FileStream fs = File.Create(fileName))
+                {
+                    // Add some text to file
+                    byte[] author = new UTF8Encoding(true).GetBytes("CICD First Attempt");
+                    fs.Write(author, 0, author.Length);
+                }
+
+                //// Open the stream and read it back.
+                //using (StreamReader sr = File.OpenText(fileName))
+                //{
+                //    string s = "";
+                //    while ((s = sr.ReadLine()) != null)
+                //    {
+                //        Console.WriteLine(s);
+                //    }
+                //}
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
         }
     }
 }
